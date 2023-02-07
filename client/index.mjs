d59961eabb6705954f526146d754dd046517e8b3
William Laker <will.laker@sky.com>
2023-02-07 13:58:24 +0000

    create counter creation in  Resource class

--- client/index.mjs
+++ client/index.mjs
@@ -1,12 +1,40 @@
 class Resource {
-    constructor(i) {
-        this.title = `Resource ${i}`
-        this.counter = 0 
-        this.plus = null
-        this.neg = null
+    constructor() {
+        this.title = null;
+        this.counter = null; 
+        this.add = null;
+        this.sub = null;
     }
 
-    
+    createCounter(i) {
+        const location = document.querySelector("main")
+
+        const section = document.createElement("section");
+        section.textContent = `Resource ${i}`;
+        section.id = `sect${i}`;
+        location.append(section);
+        this.title = section.id
+
+        const counter = document.createElement("p");
+        counter.textContent = "0";
+        counter.id = `counter${i}`;
+        section.append(counter);
+        this.counter = counter.id 
+
+        const addButton = document.createElement("button");
+        addButton.textContent = "+";
+        addButton.id = `add${i}`;
+        section.append(addButton);
+        addButton.addEventListener('click', increaseCounter);
+        this.add = addButton.id
+
+        const subButton = document.createElement("button");
+        subButton.textContent = "-";
+        subButton.id = `minus${i}`;
+        section.append(subButton);
+        subButton.addEventListener('click', decreaseCounter);
+        this.sub = subButton.id
+    }
 
 }
 
@@ -16,31 +44,11 @@ function init() {
 }
 
 function buildApp() {
-    const location = document.querySelector("main")
-
     for (let i=1; i <= 5; i++) {
-        const section = document.createElement("section");
-        section.textContent = `Resource ${i}`;
-        section.id = `sect${i}`;
-        location.append(section);
-
-        const counter = document.createElement("p");
-        counter.textContent = "0";
-        counter.id = `counter${i}`;
-        section.append(counter);
-        
-        const addButton = document.createElement("button");
-        addButton.textContent = "+";
-        addButton.id = `add${i}`;
-        section.append(addButton);
-        addButton.addEventListener('click', increaseCounter);
-
-        const subButton = document.createElement("button");
-        subButton.textContent = "-";
-        subButton.id = `minus${i}`;
-        section.append(subButton);
-        subButton.addEventListener('click', decreaseCounter);
+        let newCounter = new Resource;
+        newCounter.createCounter(i);
     }
+
 }
 
 function increaseCounter(event) {  
