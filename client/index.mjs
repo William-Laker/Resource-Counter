ad7ec7f39c537ae6d20366d5b24d50561399f5d9
William Laker <will.laker@sky.com>
2023-02-08 11:26:30 +0000

    basic functionality complete

--- client/index.mjs
+++ client/index.mjs
@@ -16,7 +16,7 @@ class Resource {
         this.title = section.id
 
         const counter = document.createElement("p");
-        counter.textContent = "0";
+        counter.textContent = 0;
         counter.id = `counter${i}`;
         section.append(counter);
         this.counter = counter.id 
@@ -52,18 +52,18 @@ function buildApp() {
 
 function changeCounter() {  
     const id = this.id;
-    const operation = id[0]
-    console.log(operation)
-    const numb = id[3]
+    const operation = id[0];
+    const numb = id[3];
     
-    const counter = document.querySelector(`#counter${numb}`)
-    console.log(counter)
+    const counter = document.querySelector(`#counter${numb}`);
     if (operation === "a") {
-        counter.textContent += 1;
+        const currentValue = ++counter.textContent;
+        counter.textContent = currentValue;
     }
     
     else if (operation === "s") {
-        counter.textContent -= 1;
+        const currentValue = --counter.textContent;
+        counter.textContent = currentValue;
     }
 }
 
