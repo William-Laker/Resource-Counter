da247069b8984584c1dfb572fbd019d59e870ded
William Laker <will.laker@sky.com>
2023-02-10 14:25:31 +0000

    refactoring

--- client/index.mjs
+++ client/index.mjs
@@ -9,32 +9,28 @@ function createCounter(i) {
     section.textContent = `Resource ${i}`;
     section.id = `sect${i}`;
     location.append(section);
-    this.title = section.id
 
     const counter = document.createElement("p");
     counter.textContent = 0;
     counter.id = `counter${i}`;
     section.append(counter);
-    this.counter = counter.id 
 
     const addButton = document.createElement("button");
     addButton.textContent = "+";
     addButton.id = `add${i}`;
     section.append(addButton);
     addButton.addEventListener('click', changeCounter);
-    this.add = addButton.id
 
     const subButton = document.createElement("button");
     subButton.textContent = "-";
     subButton.id = `sub${i}`;
     section.append(subButton);
     subButton.addEventListener('click', changeCounter);
-    this.sub = subButton.id
 }
 
 function buildApp() {
     for (let i=1; i <= 5; i++) {
-        createCounter
+        createCounter(i);
     }
 }
 
